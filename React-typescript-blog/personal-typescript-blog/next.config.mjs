f50ed83c06f0a85b0c5cc3fb73d6f2fe2825ca17
Thomas Wasnidge <thomas.wasnidge@gmail.com>
2024-03-22 22:36:31 +0000

    remove popover

--- React-typescript-blog/personal-typescript-blog/next.config.mjs
+++ React-typescript-blog/personal-typescript-blog/next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    reactStrictMode: false,
+    images: {
+        dangerouslyAllowSVG: true,
+        domains: ["tailwindui.com", "images.unsplash.com", "eincode.com", "thrangra.sirv.com"]
+      }
+};
 
 export default nextConfig;
