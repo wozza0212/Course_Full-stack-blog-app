8307354f1033fdeddbf0dc9de8827f32639e61fc
Thomas Wasnidge <thomas.wasnidge@gmail.com>
2024-03-23 18:58:27 +0000

    footer as seperate component

--- React-typescript-blog/personal-typescript-blog/src/app/page.tsx
+++ React-typescript-blog/personal-typescript-blog/src/app/page.tsx
@@ -5,6 +5,7 @@ import { Fragment } from "react";
 
 import { Popover } from "@headlessui/react";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
+import Footer from "./components/footer";
 
 const navigation = [
   { name: "Blogs", href: "/blogs" },
@@ -111,18 +112,6 @@ const socialLinks = [
     ),
   },
 ];
-
-function Copyright() {
-  return (
-    <>
-      &copy;
-      <Link legacyBehavior color="inherit" href="/">
-        XY
-      </Link>{" "}
-      {new Date().getFullYear()}
-    </>
-  );
-}
 
 const Home: NextPage = () => {
   return (
@@ -376,41 +365,7 @@ const Home: NextPage = () => {
           {/* Portfolio List Ends */}
         </div>
       </div>
-      <footer className="bg-gray-800">
-        <div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:px-8">
-          <nav
-            className="-mx-5 -my-2 flex flex-wrap justify-center"
-            aria-label="Footer"
-          >
-            {navigation.map((item) => (
-              <div key={item.name} className="px-5 py-2">
-                <Link legacyBehavior href={item.href}>
-                  <a className="text-base text-gray-400 hover:text-gray-500">
-                    {item.name}
-                  </a>
-                </Link>
-              </div>
-            ))}
-          </nav>
-          <div className="mt-8 flex justify-center space-x-6">
-            {socialLinks.map((item) => (
-              <a
-                rel="noreferrer"
-                target="_blank"
-                key={item.name}
-                href={item.href}
-                className="text-gray-400 hover:text-gray-500"
-              >
-                <span className="sr-only">{item.name}</span>
-                <item.icon className="h-6 w-6" aria-hidden="true" />
-              </a>
-            ))}
-          </div>
-          <p className="mt-8 text-center text-base text-gray-400">
-            <Copyright />, All rights reserved.
-          </p>
-        </div>
-      </footer>
+      <Footer />
     </>
   );
 };
