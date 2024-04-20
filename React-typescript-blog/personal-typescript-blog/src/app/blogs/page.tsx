35604f6c1631c001949c4d31caf80d294fbe9960
Thomas Wasnidge <thomas.wasnidge@gmail.com>
2024-04-20 12:02:40 +0100

    addinf [slug] directory

---
+++ React-typescript-blog/personal-typescript-blog/src/app/blogs/page.tsx
@@ -0,0 +1,13 @@
+import { BaseLayout } from "../components"
+
+const BlogPage = () => {
+    return (
+        <BaseLayout>
+        <div>
+            <h1>Blog Page</h1>
+        </div>
+</BaseLayout>
+    )
+}
+
+export default BlogPage
