0d728f0ee56b55dbe9560191cc85ee36f2c4e794
Thomas Wasnidge <thomas.wasnidge@gmail.com>
2024-04-20 13:48:37 +0100

    fix and add layouts

--- React-typescript-blog/personal-typescript-blog/src/app/components/index.tsx
+++ React-typescript-blog/personal-typescript-blog/src/app/components/index.tsx
@@ -4,3 +4,4 @@ export { default as Header } from './header/header';
 export { default as BlogList } from './blog/blog';
 export { default as PortfolioList } from './portfolio/portfolio-list';
 export { default as BaseLayout } from './layouts/base/base-layout';
+export { default as PageLayout } from './layouts/page/blog-detail';
